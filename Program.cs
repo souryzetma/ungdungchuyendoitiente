3bfe2e74b70f11e0b4a69583e4f01f7988d65541
unknown <souryzetma1144@yahoo.com>
2024-08-06 14:01:34 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("usd: " );
+
+        int usd = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("vnd: " + usd * 23000);
+    }
+}
